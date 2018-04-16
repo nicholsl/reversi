ea5373969334fe6ac5ede99f131d30e7a317157b
Joshua Gerstein <gersteinj@carleton.edu>
2018-04-16 11:24:47 -0500

    Cleaning up repository

--- Autocomplete/src/edu/carleton/gersteinj/Main.java
+++
@@ -1,9 +0,0 @@
-package edu.carleton.gersteinj;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.getProperties().list(System.out);
-        System.exit(0xC00000FD);
-    }
-}
