072b568718d22e38d770c43b5f4f6324c67832ab
Cameron Kline <cklinesh@gmail.com>
2018-05-14 23:06:00 -0500

    Added untested page turning function in results, and a search-bar style header

--- webapp/static/results.js
+++ webapp/static/results.js
@@ -1,24 +1,28 @@
 /*
 	Created by Cameron Kline-Sharpe and Joshua Gerstein
-	Script for /results?<GET parameters> page
+	
 	IMPORTANT CONFIGURATION INFORMATION
-	The contents of getAPISearchURL below reflects our assumption that the web 
-	application and the API will be running on the same host but on different ports.
+	The contents of getBaseURL below reflects our assumption that
+	the web application (books_website.py) and the API (books_api.py)
+	will be running on the same host but on different ports.
 	
 	TO DO:
 		Add table headings
-		Impliment 3 great circle calc functions	
+		Impliment 3 great circle calc functions
+		Impliment clean_url()
+	
 */
 
 var searchParams = new URLSearchParams(window.location.search.substring(1));
 var resultsTable = document.getElementById("results_table");
+var currentPage = flipPage(0);
 initialize();
 
 function initialize() {
 	let locationProvided = searchParams.get("latitude") && searchParams.get("longitude");
 	
 	// Send the request to the services API /services endpoint
-    fetch(getAPISearchURL(), {method: 'get'})
+    fetch(getAPISearchUrl(), {method: 'get'})
 
     // When the results come back, transform them from JSON string into
     // a Javascript object (in this case, a list of author dictionaries).
@@ -27,10 +31,22 @@ function initialize() {
     // Once you have your list of author dictionaries, use it to build
     // an HTML table displaying the author names and lifespan.
     .then(function(resultsArray) {
+		// Check to make sure the current page number isn't too high
+		// The too low case is handled in flipPage().
+		if (((currentPage + 1)*25 - 1) > resultsArray.length) {
+			currentPage -= 1;
+		}
+		
         // Build the table body.
         let tableBody = '';
-		// TODO: add in the table headings and sorting
-        for (var k = 0; k < resultsArray.length; k++) {
+		// Add in table headings
+		tableBody += "<tr>"
+		tableBody += "<th>Company Name</th>"
+		tableBody += "<th>Description</th>"
+		tableBody += "<th>Terminal</th>"
+		tableBody += "<th>Distance</th>"
+		tableBody += "</tr>"
+        for (var k = 25 * currentPage; k < 25 * (currentPage + 1) -1 && k < resultsArray.length; k++) {
             let tableRow = '<tr>';
 
 			// Add in the hypertext name of the company
@@ -63,8 +79,8 @@ function initialize() {
 			
 			// Add in the Distance (only if provided current location)
 			if (locationProvided) {
-				let serviceLat = resultsArray[k]["lat"];
-				let serviceLon = resultsArray[k]["long"];
+				let serviceLat = resultsArray[k]["latitude"];
+				let serviceLon = resultsArray[k]["longitude"];
 				tableRow += '<td>' + getUserDistanceFromCoords(serviceLat, serviceLon) + '</td>';
 			}
 			
@@ -72,6 +88,22 @@ function initialize() {
         }
 		resultsTable.innerHTML = tableBody;
 	})
+}
+
+function flipPage(change){
+	let currentPage = searchParams.get("page");
+	// Ensure that if no page number was given, there is an initial value
+	if (!currentPage) {
+		currentPage = 0;
+	} else {
+		currentPage = parseInt(currentPage);
+	}
+	currentPage += change;
+	// Ensure sure the current page does not fall below 0
+	if (currentPage < 0) {
+		currentPage = 0;
+	}
+	return currentPage;
 }
 
 function getUserDistanceFromCoords(serviceLat, serviceLon) {
@@ -84,6 +116,7 @@ function getUserDistanceFromCoords(serviceLat, serviceLon) {
 	return 100;
 }
 
+
 function removeEmptyParams(params) {
 	// Given search parameters, return a new parameters object that omits parameters with empty value
 	var newParams = new URLSearchParams(params);
@@ -95,9 +128,13 @@ function removeEmptyParams(params) {
 	return newParams;
 }
 
-function getAPISearchURL(){
-	// Generate URL for the API based on filtered GET parameters of current page.
+function getAPISearchUrl(){
+	// Generate URL for the API based on GET parameters of current page.
 	let filteredParams = removeEmptyParams(searchParams);
-	var APIBaseURL = window.location.protocol + '//' + window.location.hostname + ':' + api_port;
-	return APIBaseURL + "/services?" + filteredParams.toString();
+	return getAPIBaseURL() + "/services?" + filteredParams.toString();
+}
+
+function getAPIBaseURL() {
+    var APIBaseURL = window.location.protocol + '//' + window.location.hostname + ':' + api_port;
+    return APIBaseURL;
 }
