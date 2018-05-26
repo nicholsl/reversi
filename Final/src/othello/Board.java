026207390d9740de403df520c6009e65f08cbfd7
Joshua Gerstein <gersteinj@carleton.edu>
2018-05-25 22:53:47 -0500

    Made template for Board class

--- Final/src/othello/Board.java
+++ Final/src/othello/Board.java
@@ -1,4 +1,37 @@
 package edu.carleton.gersteinj.othello;
 
+/**
+ * Board state will be kept track of with an 8x8x3 matrix - columns, rows, content
+ */
 public class Board {
+    /*
+     Subclass for board coordinates
+     */
+    public class Coordinates {
+        int x;
+        int y;
+
+        Coordinates(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    private int[][] state;
+    public boolean blackToMove;
+
+    /*
+       Compute and return the current coordinates at which the current player could place a piece.
+     */
+    public Coordinates[] getAvailableMoves() {
+        return new Coordinates[]{new Coordinates(0, 0)};
+    }
+
+    /*
+     Applies the given move to the board's state. Return a boolean of whether or not the move was legal. If not legal
+     move, state doesn't change.
+     */
+    public boolean applyMove(Coordinates move) {
+        return false;
+    }
 }
